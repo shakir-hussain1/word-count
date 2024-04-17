577d7fb3d61270562b948b77ebda2f427781d153
Shakir Hussain <161478273+shakir-hussain1@users.noreply.github.com>
2024-04-17 16:39:36 +0500

    push word count app to github

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node 
+
+console.log(chalk.bgYellow.black.underline("WELCOME TO WORDS COUNT APP"))
+
+import inquirer from "inquirer" 
+import chalk from "chalk"
+
+const answers: 
+{
+    Sentence: string
+} = await inquirer.prompt
+(
+    [
+        {
+            name: "Sentence",
+            type: "input",
+            message: (chalk.red("Enter Your Sentence to Count the Words in Sentence: "))
+        }
+    ]
+)
+
+const words = answers.Sentence.trim().split(" ")
+
+console.log(chalk.blue(words))
+
+console.log(chalk.green(`You have ${words.length} words in your sentence which you entered.`));
